732428885bf072ca1c2ddba194e4f27b7b54ef42
Taro Morita <morita0079@gmail.com>
2021-09-06 18:32:21 +0900

    本番設定 html 若干変更

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func strTof64(text string) float64 {
 }
 
 func stringToTime(text string) time.Time {
-	var layout = "2006-01-02T15:04Z"
+	var layout = "2006-01-02T15:04"
 	t, _ := time.Parse(layout, text)
 	return t
 }
